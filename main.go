eee53fda386e8821d0c679e182696e4a2cfeebb2
Bode Falade <bbfalade@spscommerce.com>
2019-07-03 13:50:06 -0500

    intial commit.

---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+type (
+	// todoModel describes a todoModel type
+	todoModel struct {
+		gorm.Model
+		Title     string `json:"title"`
+		Completed int    `json:"completed"`
+	}
+
+	// transformedTodo represents a formattee todo
+	transformedTodo struct {
+		ID        uint   `json:"id`
+		Title     string `json:"title"`
+		Completed bool   `json:"completed"`
+	}
+)
+
+var db *gorm.DB
+
+func init() {
+	///open a db connection
+	var err error
+	db, err = gorm.Open("mysql", `root:12345@/demo?
+	charset=utf&parseTime=True&loc=Local`)
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&todoModel{})
+}
+
+// createTodo add a new todo
+func createTodo(c *gin.Context) {
+	completed, _ := strconv.Atoi(c.PostForm("completed"))
+	todo := todoModel{Title: c.PostForm("title"), Completed: completed}
+	db.Save(&todo)
+	c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated,
+		"message": "Todo item created successfully!", "resourceId": todo.ID})
+}
+
+// fetchAllTodo fetch all todos
+func fetchAllTodo(c *gin.Context) {
+	var todos []todoModel
+	var _todos []transformedTodo
+
+	db.Find(&todos)
+
+	if len(todos) <= 0 {
+		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound,
+			"message": "No todo found!"})
+		return
+	}
+
+	// transforms the todos for building a good response
+	for _, item := range todos {
+		completed := false
+		if item.Completed == 1 {
+			completed = true
+		} else {
+			completed = false
+		}
+		_todos = append(_todos, transformedTodo{ID: item.ID, Title: item.Title, Completed: completed})
+	}
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": _todos})
+}
+
+func main() {
+
+	router := gin.Default() // similar to app = express();
+
+	v1 := router.Group("/api/v1/todos")
+	{ // setup router
+		v1.POST("/", createTodo)
+		v1.GET("/", fetchAllTodo)
+		v1.GET("/:id", fetchSingleTodo)
+		v1.PUT("/:id", updateTodo)
+		v1.DELETE("/:id", deleteTodo)
+	}
+	router.Run
+}
